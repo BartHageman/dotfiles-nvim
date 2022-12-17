1b5583f6b4b00110ffecf8f1ec08829bfe0324bc
Bart Hageman <barthageman1@gmail.com>
2022-12-17 14:06:26 +0100

    STYLE: Change icons on lualine

--- neovim/.config/nvim/lua/BartH/setups/lualine.lua
+++ neovim/.config/nvim/lua/BartH/setups/lualine.lua
@@ -2,7 +2,7 @@
         options = {
             theme = 'gruvbox-material',
             -- section_separators = { left = '', right = ''},
-            section_separators = { left = '', right = ''}
-            -- component_separators = { left = '|', right = '|'}
+            section_separators = { left = '', right = ''},
+            component_separators = { left = '', right = ''}
     }
 }
