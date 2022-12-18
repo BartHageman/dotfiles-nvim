9b66f4a8c806207f979a10c600252dc73ee04917
Bart Hageman <barthageman1@gmail.com>
2022-12-18 14:20:53 +0100

    FIX: Fix neovide transparency on all platforms

--- neovim/.config/nvim/lua/BartH/colorscheme.lua
+++ neovim/.config/nvim/lua/BartH/colorscheme.lua
@@ -2,7 +2,7 @@
 function ColorMeSurprised(color)
     color = color or "gruvbox-material"
     vim.cmd.colorscheme(color)
-    if vim.fn.has("win32") ~= 1 then
+    if vim.fn.has("win32") ~= 1 and not vim.g.neovide then
         vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
         vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
     end
