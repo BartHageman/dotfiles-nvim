778caf55fdf54a10ddeb68db7c3df0c0fd936037
Bart Hageman <barthageman1@gmail.com>
2025-02-05 13:56:45 +0100

    Add incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-	config = function()
-		require'nvim-treesitter.configs'.setup {
-			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-			auto_install = false,
-
-			highlight = {
-				enable = true,
-				disable = function(lang, buf)
-					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
-		}
-	end,
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      modules = {},
+      sync_install = false,
+      ignore_install = { "javascript" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+      auto_install = false,
+      highlight = {
+        enable = true,
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = "<Tab>",
+          node_decremental = "<S-Tab>",
+        },
+      },
+    }
+  end,
 }
