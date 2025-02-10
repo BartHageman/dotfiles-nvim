639bd39f671cc7f17ac3ed4f41cfd5d1b52dab7b
Bart Hageman <barthageman1@gmail.com>
2025-02-10 21:22:35 +0100

    update colorscheme

--- lua/custom/utils.lua
+++ lua/custom/utils.lua
@@ -8,4 +8,29 @@ M.PartialApplication = function(func, defaults)
   return final_func
 end
 
+M.PatternMatch = function(patterns, default)
+  local p = function(query)
+    local patterns_meta = {
+      _index = default or function(key)
+        vim.notify("Error: Pattern '" .. key .. "' could not be matched!")
+      end
+    }
+    setmetatable(patterns, patterns_meta)
+    return patterns[tostring(query)]
+  end
+  return p
+end
+
+local p = M.PatternMatch({ ["hello"] = function() print("hello world!") end })
+
+print("dave")
+p("dave")()
+-- local patterns = { ["hello"] = function() print("hello world!") end }
+-- print(vim.inspect(patterns))
+-- print(vim.inspect(patterns["hello"]))
+
+-- p("hello")
+-- p("blah")
+
+
 return M
