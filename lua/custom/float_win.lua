964b5d13c4dc92d399c5ea99cd39e307634aea12
Bart Hageman <barthageman1@gmail.com>
2025-07-24 17:42:46 +0200

    undo terminal oopsie

--- lua/custom/float_win.lua
+++ lua/custom/float_win.lua
@@ -73,7 +73,7 @@ function M.create_terminal(config)
   if create_fresh and config.cmd then
     vim.fn.termopen(config.cmd)
   elseif create_fresh then
-    vim.fn.termopen()
+    vim.fn.termopen(vim.o.shell)
   end
   -- Start terminal in the buffer
 
