fbeb6b19b556c3c498ea266b074ee822c26761c0
Bart Hageman <barthageman1@gmail.com>
2025-07-25 13:00:11 +0200

    Add leap.nvim

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,7 @@
+return{
+    'ggandor/leap.nvim',
+    config = function()
+      vim.keymap.set({'n', 'x', 'o'}, 'ss', '<Plug>(leap)')
+      vim.keymap.set('n',             'SS', '<Plug>(leap-from-window)')
+    end
+}
