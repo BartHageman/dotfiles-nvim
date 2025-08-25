7cc6d91074e6384542fba94196c654807794d0c6
Bart Hageman <bart.hageman@rdi.nl>
2025-08-25 19:31:00 +0200

    make mini files nicer

--- lua/plugins/mini_files.lua
+++ lua/plugins/mini_files.lua
@@ -3,7 +3,19 @@ return {
     'echasnovski/mini.files',
     version = '*',
     config = function()
-      require('mini.files').setup()
+      require('mini.files').setup {
+        windows = {
+          -- Maximum number of windows to show side by side
+          max_number = 1,
+          -- Whether to show preview of file/directory under cursor
+          preview = true,
+          -- Width of focused window
+          width_focus = 80,
+          -- Width of non-focused window
+          width_nofocus = 15,
+          -- Width of preview window
+          width_preview = 25,
+        } }
       vim.keymap.set('n', '<leader>te', MiniFiles.open)
     end
   },
