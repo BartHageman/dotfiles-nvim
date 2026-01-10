7ec344c2cf4b7433cbfcbf75ea90dc63f8eac081
bart <bart.hageman@rdi.nl>
2026-01-10 21:34:29 +0100

    uv run python instead

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,4 +1,4 @@
 local tt = require("custom.float_win")
 vim.keymap.set({ 'n', 't' }, "<leader>tr", function()
-  tt.toggle_terminal { title = "Python Run", cmd = "python " .. vim.fn.expand('%') }
+  tt.toggle_terminal { title = "Python Run", cmd = "uv run python " .. vim.fn.expand('%') }
 end)
