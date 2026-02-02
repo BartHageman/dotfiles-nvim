f1f9ea095ee3eeca87719bd4f440b5bc381fa179
bart <bart.hageman@rdi.nl>
2026-02-02 13:14:12 +0100

    Replace leap with flash

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,7 +1,8 @@
-return{
-    'ggandor/leap.nvim',
-    config = function()
-      vim.keymap.set({'n', 'x', 'o'}, 'ss', '<Plug>(leap)')
-      vim.keymap.set('n',             'SS', '<Plug>(leap-from-window)')
-    end
-}
+return {}
+-- return{
+--     'ggandor/leap.nvim',
+--     config = function()
+--       vim.keymap.set({'n', 'x', 'o'}, 'ss', '<Plug>(leap)')
+--       vim.keymap.set('n',             'SS', '<Plug>(leap-from-window)')
+--     end
+-- }
