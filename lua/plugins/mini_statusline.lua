0280fae1271efb67011e90c51a5785cd608200d6
Bart Hageman <barthageman@protonmail.com>
2026-02-23 19:44:16 +0100

    Move back to lualine

--- lua/plugins/mini_statusline.lua
+++ lua/plugins/mini_statusline.lua
@@ -1,5 +1,25 @@
 return {
-  'echasnovski/mini.statusline',
-  version = "*",
-  config = { use_icons = true }
+  {
+    "letieu/harpoon-lualine",
+    dependencies = {
+      {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+      }
+    },
+  },
+  {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      opts = {
+        theme = 'auto',
+        component_separators = { left = ' ', right = ' '},
+        section_separators = { left = '', right = ''},
+        sections = {
+          lualine_a = { { 'mode', separator = { left = '',  right = '' }, right_padding = 2 } },
+          lualine_x = { 'harpoon2', 'encoding', 'fileformat', 'filetype' },
+          lualine_z = { { 'location', separator = { right = '' }, right_padding = 2 } },
+        }
+      }
+  }
 }
