822bbca30090e085c0855f71686e53850547c894
bart <bart.hageman@rdi.nl>
2026-03-05 13:49:29 +0100

    Harpoon: flash buffer name on screen

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,59 @@
+local active_flash = nil
+local function flash_filename()
+  -- kill previous flash immediately
+  if active_flash and vim.api.nvim_win_is_valid(active_flash.win) then
+    vim.api.nvim_win_close(active_flash.win, true)
+    vim.api.nvim_buf_delete(active_flash.buf, { force = true })
+  end
+
+  -- Get file icons:
+  local devicons = require("nvim-web-devicons")
+
+  local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
+  local ext = vim.fn.fnamemodify(name, ":e")
+  local icon, icon_hl = devicons.get_icon(name, ext, { default = true })
+
+  local text = "  " .. icon .. " " .. name .. ""
+
+
+  --local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
+  local buf = vim.api.nvim_create_buf(false, true)
+  --vim.api.nvim_buf_set_lines(buf, 0, -1, false, { " " .. name .. " " })
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { text })
+  text_len = vim.fn.strdisplaywidth(text)
+  local width = text_len + 2
+  local even_width = math.floor((text_len + 2)/2)*2
+
+  local win = vim.api.nvim_open_win(buf, false, {
+    relative = "editor",
+    row = math.floor(vim.o.lines / 2),
+    col = math.floor((vim.o.columns - even_width) / 2),
+
+    width = width,
+    height = 1,
+    style = "minimal",
+    border = "rounded",
+    focusable = false,
+  })
+  vim.api.nvim_set_hl(0, "HarpoonFlash", { bg = "None", fg = "#c0caf5", bold = true })
+  vim.api.nvim_win_set_option(win, "winhl", "Normal:HarpoonFlash,FloatBorder:Comment")
+  active_flash = { win = win, buf = buf }
+
+  vim.defer_fn(function()
+    if active_flash and active_flash.win == win then
+      if vim.api.nvim_win_is_valid(win) then vim.api.nvim_win_close(win, true) end
+      if vim.api.nvim_buf_is_valid(buf) then vim.api.nvim_buf_delete(buf, { force = true }) end
+      active_flash = nil
+    end
+  end, 300)
+end
+
+local function harpoon_select_n_flash(number)
+  local harpoon = require("harpoon")
+  harpoon:list():select(number)
+  flash_filename()
+end
+
 return {
   {
     "ThePrimeagen/harpoon",
@@ -6,6 +62,8 @@ return {
     config = function()
       local harpoon = require("harpoon")
 
+
+
       -- REQUIRED
       harpoon:setup()
       -- REQUIRED
@@ -13,10 +71,10 @@ return {
       vim.keymap.set("n", "<leader>mm", function() harpoon:list():add() end)
       vim.keymap.set("n", "<leader>ml", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<C-h>", function() harpoon_select_n_flash(1) end)
+      vim.keymap.set("n", "<C-j>", function() harpoon_select_n_flash(2) end)
+      vim.keymap.set("n", "<C-k>", function() harpoon_select_n_flash(3) end)
+      vim.keymap.set("n", "<C-l>", function() harpoon_select_n_flash(4) end)
 
       -- vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
       -- vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
