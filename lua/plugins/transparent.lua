e646566b1f6a90d9dca25ee7e11b38fb9417a416
bart <bart.hageman@rdi.nl>
2026-03-25 16:36:18 +0100

    Merge

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -4,8 +4,6 @@ return {
   'xiyaowong/transparent.nvim',
   dependencies = {'nvim-lualine/lualine.nvim'},
   lazy = false,
-  config = function()
-    require('transparent').setup{}
-  end
+  opts = {}
   }
 }
